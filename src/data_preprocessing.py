43d127aeb9a8a44bcd04545f1923eb0a7b257b98
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2022-01-15 20:11:37 +0100

    better way of labelisation

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -40,10 +40,12 @@ class Get_and_process_data:
         self.rel_folder_name = "rel"
         self.txt_folder_name = "txt"
 
-    def preprocess(self):
+    def load_parse(self):
+        """See EDA for better understanding of this function"""
+        
         text_files = glob.glob(self.train_data_path + os.sep + self.txt_folder_name + os.sep +  "*.txt")
         filename = ""
-        df = pd.DataFrame()
+        raw_files = pd.DataFrame()
         for file in tqdm(text_files):
             with open(file, 'r') as f:
                 text = f.read()
@@ -52,14 +54,33 @@ class Get_and_process_data:
                 concept = parse_concept(self.train_data_path + os.sep + self.concept_folder_name + os.sep +  filename + ".con")
                 rel = parse_relation(self.train_data_path + os.sep + self.rel_folder_name + os.sep +  filename + ".rel")
                 
-                df = df.append(pd.DataFrame({"text": [text], "filename": [filename] , "concept": [concept], "ast": [ast], "rel": [rel]}), ignore_index=True)
+                raw_files = raw_files.append(pd.DataFrame({"text": [text], "filename": [filename] , "concept": [concept], "ast": [ast], "rel": [rel]}), ignore_index=True)
         
         
-        return df
+        ast_concept_df = pd.DataFrame()
+        for i, file in raw_files.iterrows():
+            ast_dict = file["ast"]
+            concept_dict = file["concept"]
+            tmp_ast = pd.DataFrame(ast_dict)
+            tmp_ast = tmp_ast.drop(columns=["concept_type"])
+            tmp_ast = tmp_ast.rename(columns={"assertion_type": "ast_con_label"})
+
+            #Only concepts with not "problem"
+            tmp_concept = pd.DataFrame(concept_dict)
+            tmp_concept = tmp_concept[tmp_concept["concept_type"] != "problem"]
+            tmp_concept = tmp_concept.rename(columns={"concept_type": "ast_con_label"})
+            
+            tmp_ast["filename"] = file["filename"]
+            tmp_concept["filename"] = file["filename"]
+            ast_concept_df = ast_concept_df.append(tmp_ast, ignore_index=True)
+            ast_concept_df = ast_concept_df.append(tmp_concept, ignore_index=True)
+        #cols = concept_text, start_line, start_word_number, end_line, end_word_number, ast_con_label, filename
+        
+        return ast_concept_df
 
     # def get_dataset(self):
         
         
         
 if __name__ == "__main__":
-    print(Get_and_process_data().preprocess())
+    print(Get_and_process_data().load_parse())
