d2c396f2ae0fcb86231e27dae90221dc5ab74022
Nouamane Tazi <nouamane98@gmail.com>
2022-01-26 13:06:03 +0100

    add each patient page

--- src/api.py
+++ src/api.py
@@ -1,20 +1,23 @@
 from flask import Flask, redirect, url_for, request
+from utils import search_query
+import json
+
 app = Flask(__name__)
+
 
 @app.route("/hello")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
-@app.route("/indexes/train-index/docs/search", methods=['POST'])
-def search():
-    # data = pd.read_excel('C:/Users/adsieg/Desktop/flask_tuto/wine_filter.xlsx', encoding='utf8')
-    # data = data[(data['country']==request.form["Country"]) & (data['province']==request.form["Region"])]
-    # data = data.head(50)
-    # data = data.to_dict(orient='records')
-    # response = json.dumps(data, indent=2)
+
+@app.route("/indexes/train-index/docs/search", methods=["POST"])
+def main():
     request_data = request.get_json()
-    print(request_data)
-    return {}
+    records = search_query(query=request_data["query"], filters=request_data["filters"], top=request_data["top"])
+    assert len(set([record["filename"] for record in records])) == len(records), "filenames of results are not unique"
+    res = {"value": records, "count": len(records)}
+    return json.dumps(res, indent=2)
+
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000)
+    app.run(host="localhost", port=5000)
--- src/utils/search.py
+++ src/utils/search.py
@@ -1,4 +1,17 @@
-
-
-def search():
-    
+def search_query(query, filters={}, top=10):
+    return [
+        {
+            "filename": f"018636330_DH_{i}.txt",
+            "preview": """DISCHARGE MEDICATIONS :
+She was discharged on the following medications .
+1. Colace , 100 mg PO bid .
+2. Zantac , 150 mg PO bid .""",
+            "metadata": {
+                "age": 30,
+                "sexe": "F",
+                "birthdate": "01/01/1980",
+                "admission_date": "01/01/2020",
+                "discharge_date": "01/01/2021",
+            },
+        } for i in range(100)
+    ]
